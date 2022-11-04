03e0c667d7a926eb1879e526939a52fbefcdbb76
nesterio <nesterio619@gmail.com>
2022-11-04 15:16:21 +0100

    Improve Grabbable structure

--- Assets/Scripts/Grabbable.cs
+++ Assets/Scripts/Grabbable.cs
@@ -1,60 +1,38 @@
-using DG.Tweening;
 using UnityEngine;
 
 public class Grabbable : MonoBehaviour
 {
-    [Header("Components")]
-    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] protected Rigidbody _rigidbody;
 
     private bool _isInitialized;
 
-    private bool _holdingSmth;
-
-    private readonly InputManager _inputManager = InputManager.Instance;
+    protected readonly InputManager _inputManager = InputManager.Instance;
     
-    private readonly float _speed = 7.5f;
+    protected readonly float _speed = 7.5f;
 
-    private Tween _moveTween;
+    private void Awake() => Initialize();
 
-    private void Awake()
-    {
-        Initialize();
-    }
-    
     private void Initialize()
     {
         if (_rigidbody == null)
             _rigidbody = GetComponent<Rigidbody>();
         
         if (_rigidbody == null)
-        { 
             _rigidbody = gameObject.AddComponent(typeof(Rigidbody)) as Rigidbody;
-        }
     }
     
-    public void Lift()
+    public virtual void Lift()
     {
         _inputManager.MousePosEvent += Move;
         _inputManager.ReleaseEvent += Release;
-            
-        if (!_holdingSmth)
-            _holdingSmth = true;
     }
 
-    private void Release()
+    protected virtual void Release()
     {
         _inputManager.MousePosEvent -= Move;
         _inputManager.ReleaseEvent -= Release;
-
-        if (_holdingSmth)
-            _holdingSmth = false;
     }
 
-    void Move(Vector3 vector3)
-    {
-        _rigidbody.velocity = (vector3 - transform.position) * _speed;
-    }
-    
-    
+    void Move(Vector3 vector3) => _rigidbody.velocity = (vector3 - transform.position) * _speed;
 
 }
