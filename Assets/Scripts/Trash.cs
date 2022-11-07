014c9c29a935ce664b38a2d79136cd4b690f84bc
RuslanVelikazov <115423048+RuslanVelikazov@users.noreply.github.com>
2022-11-07 14:46:23 +0300

    TrashClass

--- Assets/Scripts/Trash.cs
+++ Assets/Scripts/Trash.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class Trash : Grabbable
 {
     [SerializeField] GameObject trashObject;
-    [SerializeField]private float jumpingAbilityTrash;
+    [SerializeField] Collider _collider;
+    private PhysicMaterial _physicMaterial;
+    [SerializeField]private float Bounciness;
     [SerializeField]private float massTrash;
+
 
     private void Start()
     {
         trashObject.tag = "Trash";
 
         _rigidbody.mass = massTrash;
-        _rigidbody.drag =jumpingAbilityTrash;
+
+        PhysicMaterial trashMaterial = new PhysicMaterial();        
+        trashMaterial.bounciness = Bounciness;
+        _collider.material = trashMaterial;
+        _physicMaterial = trashMaterial;
     }
 }
 enum TrashType
