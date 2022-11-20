885d41175eaaacf9f9448cb2c541130a6d00455f
RuslanVelikazov <115423048+RuslanVelikazov@users.noreply.github.com>
2022-11-20 16:15:12 +0300

    BugFix

--- Assets/Scripts/Managers/TrashRecievingWindow.cs
+++ Assets/Scripts/Managers/TrashRecievingWindow.cs
@@ -7,23 +7,23 @@ public class TrashRecievingWindow : MonoBehaviour
     [SerializeField] Collider trashBox;
 
     private float initalDuration = 3;
-    private float currentDuration;
+    private float _currentDuration;
     private int level = 1;
 
     public TrashType trashType;
 
     private void Start()
     {
-        currentDuration = initalDuration - initalDuration * level / 10;
+        _currentDuration = initalDuration - initalDuration * level / 10;
     }
 
     private void OnTriggerEnter(Collider col)
     {
         if (col.gameObject.tag == "Trash") //Add a click check
         {
-            if (col.gameObject.tag == "Trash")
+            if (col.gameObject.CompareTag("Trash"))
             {
-                Destroy(col.gameObject);
+                col.gameObject.SetActive(false);
                 trashBox.isTrigger = false;
                 StartCoroutine(TimerTrashBox());
                 Debug.Log("Is trash");
@@ -43,7 +43,7 @@ public class TrashRecievingWindow : MonoBehaviour
     IEnumerator TimerTrashBox()
     {
         ScoreManager.Instance.AddScore();
-        yield return new WaitForSeconds(currentDuration);
+        yield return new WaitForSeconds(_currentDuration);
         Debug.Log("Box ready");
         trashBox.isTrigger = true;
     }
