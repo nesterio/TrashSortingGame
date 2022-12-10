ace111fdbee9bdbe00cd77c349d3f8d22dc8a0ca
RuslanVelikazov <115423048+RuslanVelikazov@users.noreply.github.com>
2022-12-10 14:15:55 +0300

    BugFix ResolutionManager

--- Assets/Scripts/Managers/ResolutionManager.cs
+++ Assets/Scripts/Managers/ResolutionManager.cs
@@ -3,13 +3,16 @@ using UnityEngine;
 
 public class ResolutionManager
 {
-    public float CameraPosZ = (float)-((float)(Screen.width) / (float)(Screen.height) * ((2.16533333333 / 5.5)/(1.333 / 6.5)));
+    public float CameraPosZ = (float)-((float)(Screen.width) / (float)(Screen.height)
+                                      * ((1.333 / 6.5) / (2.16533333333 / 5.5)));
+    
 
     public void SetCameraPosition(float CameraPosZ)
     {
         var transformPosition = Camera.main.transform.position;
         transformPosition.z = CameraPosZ;
         Camera.main.transform.position = transformPosition;
+        Debug.Log("Camera Position:" + CameraPosZ);
     }
 }
 
